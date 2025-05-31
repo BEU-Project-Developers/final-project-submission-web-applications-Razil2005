301eb88b9be46efb63db21f96666a31c027b808b
Razil2005 <razilcavadov155@gmail.com>
2025-05-31 22:38:46 +0400

    some changes

--- FurnitureShopProjectRazil/obj/Debug/net8.0/FurnitureShopProjectRazil.AssemblyInfo.cs
+++ FurnitureShopProjectRazil/obj/Debug/net8.0/FurnitureShopProjectRazil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FurnitureShopProjectRazil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90313e2f761f26763267cac0c00700ffe41a1080")]
 [assembly: System.Reflection.AssemblyProductAttribute("FurnitureShopProjectRazil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FurnitureShopProjectRazil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
